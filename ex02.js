8ddd8dab386d18db99aec817610e316b819c5124
Aymene Bousbia <aymene01@hotmail.fr>
2020-10-16 13:38:41 +0200

    ex02

--- ex02.js
+++ ex02.js
@@ -1,9 +1,16 @@
-const push =(array,...e)=>{ 
-    for(let i = 0; i < array.length ; i++){
-      if (e < array[i]){
-        array.splice(i, 0, e)
-      }
-    }
-    return array
+const insertSort =(array)=>{
+  for(let i =1 ; i < array.length ; i++){
+    let insertElement = array[i];
+    let k = i-1;
+   while (insertElement < array[k] && k>=0){
+    array[k+1] = array[k]
+    k--
+  }
+  array[k+1] = insertElement
 }
+return array
+}
+
+
+
 
